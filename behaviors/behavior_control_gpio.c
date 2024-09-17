3e0fb0d647f266c9311b1a56487a69c7010a8694
Andrew Kannan <andrew.kannan@gmail.com>
2024-09-17 01:49:45 -0400

    simplify and use gpio functions for everything

--- behaviors/behavior_control_gpio.c
+++ behaviors/behavior_control_gpio.c
@@ -4,7 +4,6 @@
 #include <zephyr/device.h>
 #include <drivers/behavior.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/drivers/led.h>
 #include <zephyr/drivers/gpio.h>
 
 #include <zmk/behavior.h>
@@ -16,13 +15,10 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
 #define CHOSEN_DEVICE DT_CHOSEN(DT_DRV_COMPAT)
-
-static const struct device *const control_gpio_dev = DEVICE_DT_GET(CHOSEN_DEVICE);
-
-#define DT_GPIO_PIN_GPIOS(n) GPIO_DT_SPEC_GET(n, gpios)
+#define DT_GPIO_SPEC_GPIOS(n) GPIO_DT_SPEC_GET(n, gpios)
 
 static const struct gpio_dt_spec gpio_devices[] = {
-    DT_FOREACH_CHILD_SEP(CHOSEN_DEVICE, DT_GPIO_PIN_GPIOS , (,))
+    DT_FOREACH_CHILD_SEP(CHOSEN_DEVICE, DT_GPIO_SPEC_GPIOS , (,))
 };
 
 #define NUM_CONTROLLED_GPIO ARRAY_SIZE(gpio_devices)
@@ -34,7 +30,7 @@ int zmk_gpio_on(int idx){
     if (idx >= NUM_CONTROLLED_GPIO){
         return -1;
     } 
-    led_on(control_gpio_dev, idx);
+    gpio_pin_set_dt(&gpio_devices[idx], 1);
     return 0;
 }
 
@@ -42,7 +38,7 @@ int zmk_gpio_off(int idx){
     if (idx >= NUM_CONTROLLED_GPIO){
         return -1;
     } 
-    led_off(control_gpio_dev, idx);
+    gpio_pin_set_dt(&gpio_devices[idx], 0);
     return 0;
 }
 
@@ -83,11 +79,11 @@ static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
 static int control_gpio_init(const struct device *dev) {
     //hardcode - first GPIO turns on
     if(NUM_CONTROLLED_GPIO > 0){
-        led_on(control_gpio_dev, 0);
+        gpio_pin_set_dt(&gpio_devices[0], 1);
     }
     //hardcode- other GPIOs turn off
     for(uint8_t i=1;i<NUM_CONTROLLED_GPIO;i++){
-        led_off(control_gpio_dev, i);
+        gpio_pin_set_dt(&gpio_devices[i], 0);
     }
     return 0;
 };
