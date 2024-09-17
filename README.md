48039f2abfc8822909b434cbc59e810ffb3d2e90
Andrew Kannan <andrew.kannan@gmail.com>
2024-09-17 12:01:45 -0400

    uncouple from LED and enable default mode

--- README.md
+++ README.md
@@ -3,19 +3,17 @@ GPIO toggle behavior in ZMK
 
 Add `#include "control_gpio_behavior.dtsi"` to your keyboard's DTS file
 
-Define a gpio-leds compatible in your dts
-Mark it as chosen zmk,control-gpio
+Define a zmk.control-gpio compatible in your dts
+
+gpios are mandatory for each child, starting-state-on defines the initial state of the GPIO at boot time
 
 Example:
 ```
-    chosen {
-        zmk,control-gpio = &leds;
-    };
-
     leds: leds {
-        compatible = "gpio-leds";
+        compatible = "zmk,control-gpio";
         chg_led: led_0 {
             gpios = <&gpio1 12 GPIO_ACTIVE_HIGH>;
+            starting-state-on;
         };
         blue_led: led_1 {
             gpios = <&gpio0 26 GPIO_ACTIVE_HIGH>;
@@ -26,9 +24,10 @@ Example:
 
 This will enable the following keypress behaviors:
 
-&gpc GP_ON (index)
-&gpc GP_OFF (index)
-&gpc GP_TOG (index)
+- &gpc GP_ON (index) - turn GPIO on
+- &gpc GP_OFF (index) - turn GPIO off
+- &gpc GP_TOG (index) - toggle GPIO
 
-The index is defined by the order your leds are defined in the chosen devicetree entry
+The index is defined by the order your GPIOS are defined in the devicetree entry
+
 In this case, chg_led would be index 0 and blue_led would be index 1
--- behaviors/behavior_control_gpio.c
+++ behaviors/behavior_control_gpio.c
@@ -1,4 +1,4 @@
-#define DT_DRV_COMPAT zmk_control_gpio
+#define DT_DRV_COMPAT zmk_behavior_control_gpio
 
 // Dependencies
 #include <zephyr/device.h>
@@ -15,11 +15,16 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
 
-#define CHOSEN_DEVICE DT_CHOSEN(DT_DRV_COMPAT)
+#define GPIO_CONTROL_NODE DT_INST(0, zmk_control_gpio)
 #define DT_GPIO_SPEC_GPIOS(n) GPIO_DT_SPEC_GET(n, gpios)
+#define DT_GPIO_DEFAULT(n) DT_PROP(n, starting_state_on)
 
 static const struct gpio_dt_spec gpio_devices[] = {
-    DT_FOREACH_CHILD_SEP(CHOSEN_DEVICE, DT_GPIO_SPEC_GPIOS , (,))
+    DT_FOREACH_CHILD_SEP(GPIO_CONTROL_NODE, DT_GPIO_SPEC_GPIOS , (,))
+};
+
+static const bool gpio_defaults[] = {
+    DT_FOREACH_CHILD_SEP(GPIO_CONTROL_NODE, DT_GPIO_DEFAULT , (,))
 };
 
 #define NUM_CONTROLLED_GPIO ARRAY_SIZE(gpio_devices)
@@ -77,20 +82,16 @@ static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
 
 // Driver Code
 // Initialization Function
-static int control_gpio_init(const struct device *dev) {
-    //hardcode - first GPIO turns on
-    if(NUM_CONTROLLED_GPIO > 0){
-        gpio_pin_set_dt(&gpio_devices[0], 1);
-    }
-    //hardcode- other GPIOs turn off
-    for(uint8_t i=1;i<NUM_CONTROLLED_GPIO;i++){
-        gpio_pin_set_dt(&gpio_devices[i], 0);
+static int behavior_control_gpio_init(const struct device *dev) {
+    for(uint8_t i=0;i<NUM_CONTROLLED_GPIO;i++){
+        gpio_pin_configure_dt(&gpio_devices[i], GPIO_OUTPUT);
+        gpio_pin_set_dt(&gpio_devices[i], gpio_defaults[i]);
     }
     return 0;
 };
 
 // API Structure
-static const struct behavior_driver_api control_gpio_driver_api = {
+static const struct behavior_driver_api behavior_control_gpio_driver_api = {
     .binding_pressed = on_keymap_binding_pressed,
     .binding_released = on_keymap_binding_released,
     .locality = BEHAVIOR_LOCALITY_GLOBAL
@@ -98,9 +99,9 @@ static const struct behavior_driver_api control_gpio_driver_api = {
 
 BEHAVIOR_DT_INST_DEFINE(0,                                                    // Instance Number (Equal to 0 for behaviors that don't require multiple instances,
                                                                               //                  Equal to n for behaviors that do make use of multiple instances)
-                        control_gpio_init, NULL,                           // Initialization Function, Power Management Device Pointer
+                        behavior_control_gpio_init, NULL,                           // Initialization Function, Power Management Device Pointer
                         NULL, NULL,       // Behavior Data Pointer, Behavior Configuration Pointer (Both Optional)
                         POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,     // Initialization Level, Device Priority
-                        &control_gpio_driver_api);                         // API Structure
+                        &behavior_control_gpio_driver_api);                         // API Structure
 
 #endif /* DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT) */
